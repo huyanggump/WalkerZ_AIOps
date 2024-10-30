ca15a1c8b5b60ec274d5ed563f90aeac093f2233
WalkerZ <orangejunehuunt@163.com>
2024-10-30 19:50:16 +0800

    Add application file

---
+++ app.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+# @Author: WalkerZ
+# @Time: 2024/10/30
+
+import streamlit as st
+# hide side menu
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+st.title("Hello World!")
